c4aab08f42fadd436e6b460560bcb80ae4986d6f
Jonas Gao <gaopinsong@live.cn>
2024-02-06 21:22:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,36 +1,9 @@
 #!/bin/bash
-if [ -z "$MY_CONFIG_HOME" ]
-then
-  echo "There is no MY_CONFIG_HOME" >&2
-  exit 1
-fi
-
-CONF_SRC="$MY_CONFIG_HOME/vim/vim/plug"
-
-install_plug() {
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  echo -e "\e[32mSuccess install to ~/.vim/autoload/plug.vim\e[0m"
-}
-
-restore_conf() {
-  SRC="$CONF_SRC/.vimrc"
-  DST="$HOME/.vimrc"
-  cp "$SRC" "$DST"
-  printf "\e[32mAlready overwrite %s by %s\e[0m\n" "$SRC" "$DST"
-}
-
-while getopts "ic" OPT
-do
-  case "$OPT" in
-    i)
-      install_plug
-      ;;
-    c)
-      restore_conf
-      ;;
-    ?)
-      echo "Flags: -ic" >&2
-      ;;
-  esac
-done
+[ -z "$MY_CONFIG_HOME" ] && echo "There is no MY_CONFIG_HOME" >&2 && exit 1
+echo -e "Install vim plug..."
+PLUG_TARGET="$HOME/.vim/autoload/plug.vim"
+[ -f "$PLUG_TARGET" ] && echo -e "\033[31mAlready exists plug.vim.\033[0m" && exit 2
+[ -n "$GIT_MIRROR" ] && echo "Using GIT_MIRROR: $GIT_MIRROR"
+curl -fLo $PLUG_TARGET --create-dirs \
+  "${GIT_MIRROR}https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+echo -e "\033[32mSuccess install to ~/.vim/autoload/plug.vim\033[0m"
